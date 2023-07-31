ea33f46b46cf48b99703f85f67fe114649f28ea7
Dmitry Belyakov <dmitry.belyakov@aliexpress.ru>
2023-07-31 14:51:28 +0300

    added MarshalJSON methods in custom datetime types

--- objects/datetime.go
+++ objects/datetime.go
@@ -1,8 +1,14 @@
 package objects
 
 import (
+	"fmt"
 	"strconv"
 	"time"
+)
+
+const (
+	dateLayout = "\"2006-01-02\""
+	timeLayout = "\"2006-01-02T15:04:05.999-0700\""
 )
 
 type Time time.Time
@@ -11,12 +17,20 @@ func (t *Time) UnmarshalJSON(b []byte) error {
 	if string(b) == "null" {
 		return nil
 	}
-	ti, err := time.Parse("\"2006-01-02T15:04:05.999-0700\"", string(b))
+	ti, err := time.Parse(timeLayout, string(b))
 	if err != nil {
 		return err
 	}
 	*t = Time(ti)
 	return nil
+}
+
+func (t *Time) MarshalJSON() ([]byte, error) {
+	baseTime := time.Time(*t)
+	if baseTime.IsZero() {
+		return nil, nil
+	}
+	return []byte(fmt.Sprintf(`"%s"`, baseTime.Format(timeLayout))), nil
 }
 
 type Date time.Time
@@ -25,12 +39,20 @@ func (d *Date) UnmarshalJSON(b []byte) error {
 	if string(b) == "null" {
 		return nil
 	}
-	ti, err := time.Parse("\"2006-01-02\"", string(b))
+	ti, err := time.Parse(dateLayout, string(b))
 	if err != nil {
 		return err
 	}
 	*d = Date(ti)
 	return nil
+}
+
+func (d *Date) MarshalJSON() ([]byte, error) {
+	date := time.Time(*d)
+	if date.IsZero() {
+		return nil, nil
+	}
+	return []byte(fmt.Sprintf(`"%s"`, date.Format(dateLayout))), nil
 }
 
 type Timestamp time.Time
@@ -42,4 +64,8 @@ func (t *Timestamp) UnmarshalJSON(b []byte) error {
 	}
 	*t = Timestamp(time.Unix(i, 0))
 	return nil
+}
+
+func (t *Timestamp) MarshalJSON() ([]byte, error) {
+	return []byte(strconv.FormatInt(time.Time(*t).Unix(), 10)), nil
 }
